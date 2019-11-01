283e028ba44913763bb0b2981d2a0eda90e5d5c7
Anna Tomyak <tomyak.anna@gmail.com>
2019-11-01 22:54:24 +0200

    QuadraticEquationChecker

--- src/main/java/com/hillel/lecture_3/QuadraticEquationChecker.java
+++ src/main/java/com/hillel/lecture_3/QuadraticEquationChecker.java
@@ -9,23 +9,21 @@ public class QuadraticEquationChecker {
 
     @Step
     public String checkQuadraticEquation(double a, double b, double c) {
-//        TODO implements result
+
+
         String result = "";
-        double discriminant=Math.pow(b,2)-4*a*c;
-        if (discriminant>0&& a!=0){
-            double x1=((-b+Math.sqrt(discriminant))/2*a);
-            double x2=((-b-Math.sqrt(discriminant))/2*a);
-            result="Discriminant: "+discriminant+", x1: "+x1+", x2: "+x2;
-        }
-        else if (discriminant<00 && a!=0){
-            result="No roots on the set of real numbers!";
-        }
-        else if (a==0){
-            result="The 'a' coefficient should not be zero!";
-        }
-        else {
-            double x1=-b/2*a;
-            result="Two real, identical roots: [x1 && x2] = "+x1;
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        if (discriminant > 0 && a != 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) /( 2d * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2d * a);
+            result = "Discriminant: " + discriminant + ", x1: " + x1 + ", x2: " + x2;
+        } else if (discriminant < 0 && a != 0) {
+            result = "No roots on the set of real numbers!";
+        } else if (a == 0) {
+            result = "The 'a' coefficient should not be zero!";
+        } else {
+            double x1 = -b /( 2 * a);
+            result = "Two real, identical roots: [x1 && x2] = " + x1;
 
         }
         return result;
