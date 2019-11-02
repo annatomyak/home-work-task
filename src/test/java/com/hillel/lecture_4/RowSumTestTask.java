c91a8d5eb88c7e7c4f72105bf213a1ef1de72564
Anna Tomyak <tomyak.anna@gmail.com>
2019-11-02 17:33:20 +0200

    rowSumTestTask

--- src/test/java/com/hillel/lecture_4/RowSumTestTask.java
+++ src/test/java/com/hillel/lecture_4/RowSumTestTask.java
@@ -8,23 +8,26 @@ package com.hillel.lecture_4;
  * | 2 | 5 | 7 |
  */
 public class RowSumTestTask {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
        summOfRow();
     }
-    public static double summOfRow() {
-        double result = 0d;
-        String leftAlignFormat = "| %-15s | %-4d |%n";
+    public static int summOfRow() {
+        int result = 0;
+        String leftAlignFormat = "| %-5s | %-5d | %-5d |%n";
 
-        System.out.format("+-----------------+------+%n");
-        System.out.format("| row1    | row2     |  summ|%n");
-        System.out.format("+-----------------+------+%n");
-        for (int i = 0; i <= 360; i = i + 10) {
-            result = Math.sin(i);
-            System.out.format(leftAlignFormat, "some data" + i, result);
+        System.out.format("+------------------------+%n");
+        System.out.format("| row1  | row 2 |  summ  |%n");
+        System.out.format("+------------------------+%n");
+
+        for (int i = 0; i<10; i++){
+            int a = (int) ( Math.random() * 200-100 );
+            int b = (int) ( Math.random() * 200-100 );
+            result= a+b;
+            System.out.format(leftAlignFormat, a, b, result);
         }
         System.out.format("+-----------------+------+%n");
         return result;
 
 
-    }*/
+    }
 }
