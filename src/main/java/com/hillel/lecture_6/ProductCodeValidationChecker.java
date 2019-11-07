37272ecc196d51ca849eb059aba34d8be6f4e751
Anna Tomyak <tomyak.anna@gmail.com>
2019-11-07 23:26:06 +0200

    6 hometask

--- src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
+++ src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
@@ -14,31 +14,42 @@ public class ProductCodeValidationChecker {
     @Step
     public boolean validateCode(String code) {
 
-//        TODO implements result
         boolean isValid = false;
-        String[] arrString=code.split(" ");
-        String code1 = code.substring(0, code.indexOf(" "));
-        String code2 = code.substring(code.indexOf(" ")+1);
+        String string1 = code.substring(0, code.indexOf(" "));
+        String string2 = code.substring(code.indexOf(" ")+1);
 
-        int a=Integer.parseInt(arrString[1]);
-      String  str = arrString[0].replaceAll("\\D+","");
-     String[] arrString1=str.split("(?<=\\G.{2})");
-        int[] arrayInt = Arrays.asList(arrString1).stream().mapToInt(Integer::parseInt).toArray();
+        int leng = string1.length();
+        char[] code1Chars = string1.toCharArray();
+        String[] numbers = new String[6];
+        int counter = 0;
+        int index = 0;
 
-        int pr=1;
-        for(int x=0; x<arrayInt.length; x++) {
-            pr *= arrayInt[x];
 
+        for (int i = 0; i < code1Chars.length; i++) {
+            if (Character.isDigit(code1Chars[i])) {
+                numbers[index] = Character.toString(code1Chars[i]);
+                index++;
+                counter += 1;
+
+            }
         }
-        if (pr==a){
-            isValid=true;
 
+        int[] results = new int [6];
+        int res = 1;
+        for (int i = 0; i < numbers.length ; i++) {
+            if (i % 2 != 0) {
+                String str = numbers[i-1] + numbers[i];
+                int number = Integer.parseInt(str);
+                res = res * number;
+            }
+        }
+
+        if (string1.equals(string1.toUpperCase()) && res == Integer.parseInt(string2) && counter >= 6) {
+            isValid = true;
         }
 
 
-        else {
-            isValid=false;
-        }
+
         return isValid;
     }
 }
